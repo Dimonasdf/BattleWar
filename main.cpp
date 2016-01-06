45878c3cb42bbfb88b451bca4674d87f27fec8d5
Dimonasdf <dimonasdf@inbox.ru>
2016-01-06 21:13:52 +0300

    0.00002a
    Added exp and dmg increase.

--- main.cpp
+++ main.cpp
@@ -3,28 +3,31 @@
 #include <stdio.h>
 #include <cstdio>
 #include <time.h>
+#include <math.h>
 
-#define _hp      20
-#define _mp      20
-#define _ehp     20
+#define _hp      100                     //хп гг
+#define _mp      50                     //мп гг
+#define _ehp     100                     //хп врага
 
-#define _dmg1     1
-#define _mcos1    0
-#define _selfdmg1 0
+#define _dmg1     1                     //урон 1 способности врага
+#define _dmg2     2                     //урон 2 способности врага
 
-#define _dmg2     2
-#define _mcos2    1
-#define _selfdmg2 0
+using namespace std;                    //хз что такое, но без него не работает
 
-#define _dmg3     10
-#define _mcos3    15
-#define _selfdmg3 1
+int hp = _hp, mp = _mp, ehp = _ehp;     //инты
+//int x = 3, y = 3;                     //Х - способность, Y - значение характеристики
+int ac[3][3] =
+{
+    {  1,  0, 0 },                      //1 способность - урон врагу / мана / урон себе
+    {  2,  1, 0 },                      //2 способность - урон врагу / мана / урон себе
+    { 10, 15, 1 }                       //3 способность - урон врагу / мана / урон себе
+};
+int e1 = 0, e2 = 0;                     //опыт
+int a1 = 0, a2 = 0, a3 = 0;             //амплификаторы урона
 
-using namespace std;
-int hp = _hp, mp = _mp, ehp = _ehp;
-char action;
+char action;                            //чары
 
-int random (int _min, int _max)
+int random (int _min, int _max)         //мегарандом от Димона
 {
     int a = rand() % 1000+1;
     int b = time(NULL);
@@ -32,216 +35,227 @@ int random (int _min, int _max)
     int r = rand() % _max+_min;
     return r;
 }
-int p_attack (string weapon)
-{
-    if (weapon == "sword")
-        {
-        cout << "You sliced the enemy with your sword." << endl;
-        ehp -= _dmg1;
-        mp -= _mcos1;
-        hp -= _selfdmg1;
-        }
-    if (weapon == "magic")
-        {
-        cout << "You cast a devastating Magic Missile." << endl;
-        ehp -= _dmg2;
-        mp -= _mcos2;
-        hp -= _selfdmg2;
-        }
-    if (weapon == "ult")
-        {
-        cout << "You summoned Ultima!" << endl;
-        ehp -= _dmg3;
-        mp -= _mcos3;
-        hp -= _selfdmg3;
-        }
-    return 0;
-}
-int e_attack (string weapon)
-{
-        if (weapon == "sword")
-        {
-        cout << "The enemy used his sword." << endl << endl;
-        hp -= _dmg1;
-        }
-        if (weapon == "magic")
-        {
-        cout << "The enemy casts a Magic Missile." << endl << endl;
-        hp -= _dmg2;
-        }
-    return 0;
-}
-void win ()
+void win ()                             //вин месседж
 {
     cout << "You win." << endl << endl;
     system("pause");
 }
-void lose ()
+void lose ()                            //луз месседж
 {
     cout << "You lose." << endl << endl;
     system("pause");
 }
-void nomana ()
+void nomana ()                          //нет маны месседж
 {
     cout << "Not enough mana." << endl << endl;
     system("pause");
 }
 
-
-
-
+    int p_attack (string weapon)            //атака игрока
+{
+    if (weapon == "sword")
+        {
+        cout << "You sliced the enemy with your sword for " << ac [0][0] + a1 << " damage." << endl;
+        ehp -= ac [0][0] + a1;
+        mp -= ac [0][1];
+        hp -= ac [0][2];
+        e1 += 1;
+        cout << "Experience with swords is " << e1 << "." << endl << endl;
+        }
+    if (weapon == "magic")
+        {
+        cout << "You cast a devastating magic missile for " << ac [1][0] + a2 << " damage." << endl;
+        ehp -= ac [1][0] + a2;
+        mp -= ac [1][1];
+        hp -= ac [1][2];
+        e2 += 1;
+        cout << "Experience with magic is " << e2 << "." << endl << endl;
+        }
+    if (weapon == "ult")
+        {
+        cout << "You summoned Ultima! It dealt " << ac [2][0] + a3 << " damage to the enemy and " << ac [2][2] << " damage to you." << endl;
+        ehp -= ac [2][0] + a3;
+        mp -= ac [2][1];
+        hp -= ac [2][2];
+        e2 += 5;
+        cout << "Experience with magic is " << e2 << "." << endl << endl;
+        }
+    return 0;
+}
+    int e_attack (string weapon)            //атака врага
+{
+        if (weapon == "sword")
+        {
+        cout << "The enemy used his sword for " << _dmg1 << " damage." << endl << endl;
+        hp -= _dmg1 /*ac[0][0]*/;
+        }
+        if (weapon == "magic")
+        {
+        cout << "The enemy casts a magic missile for " << _dmg2 << " damage." << endl << endl;
+        hp -= _dmg2 /*ac[1][0]*/;
+        }
+    system("pause");
+    return 0;
+}
 
 int main()
 {
 
-label:
+while (1==1)
+{
 system("cls");
+
+a1 = e1/10;
+a2 = a3 = e2/10;
+
 cout << "Enemy HP: " << ehp << endl
 << "Your HP: " << hp << endl
 << "Your MP: " << mp << endl
 << endl
-<< "(1) Attack with sword" << endl
-<< "(2) Use magic missile" << endl
-<< "(3) Summon demon" << endl << endl;
+<< "(1) Attack with sword (Damage: " << ac[0][0] + a1 << ", Mana cost: " << ac[0][1] << ", SelfDamage: " << ac [0][2] << ")" << endl
+<< "(2) Use magic missile (Damage: " << ac[1][0] + a2 << ", Mana cost: " << ac[1][1] << ", SelfDamage: " << ac [1][2] << ")" << endl
+<< "(3) Summon demon    (Damage: " << ac[2][0] + a3 << ", Mana cost: " << ac[2][1] << ", SelfDamage: " << ac [2][2] << ")" << endl << endl;
 cin >> action;
 cout << endl << "Status:" << endl;
 
 switch (action)
 {
-case '1':
-{
-if (mp >= _mcos1)
-{
-    p_attack ("sword");
-
-    if (ehp > 0)
+    case '1':
     {
-        switch (random (1,2) + '0')
+        if (mp >= ac[0][1])
         {
-        case '1':
+            p_attack ("sword");
+
+            if (ehp > 0)
+            {
+                switch (random (1,2) + '0')
+                {
+                    case '1':
+                    {
+                        e_attack ("sword");
+                        break;
+                    }
+                    case '2':
+                    {
+                        e_attack ("magic");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                e1 += 5;
+                cout << "You smashed the enemy with your sword. Experience with swords now is " << e1 << "." << endl;
+                win();
+                return 0;
+            }
+            if (hp <= 0)
+            {
+                lose();
+                return 0;
+            }
+                break;
+        }
+        else
         {
-        e_attack ("sword");
+            nomana();
+            break;
+        }
+    }
+    case '2':
+    {
+        if (mp >= ac[1][1])
+        {
+            p_attack ("magic");
+
+            if (ehp > 0)
+            {
+                switch (random (1,2) + '0')
+                {
+                    case '1':
+                    {
+                        e_attack ("sword");
+                        break;
+                    }
+                    case '2':
+                    {
+                        e_attack ("magic");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                e2 += 5;
+                cout << "The eneny was destroyed. Experience with magic now is " << e2 << "." << endl;
+                win();
+                return 0;
+            }
+            if (hp <= 0)
+            {
+                lose();
+                return 0;
+            }
+                break;
+        }
+        else
+        {
+            nomana();
+            break;
+        }
+    }
+    case '3':
+    {
+        if (mp >= ac[2][1])
+        {
+            p_attack ("ult");
+
+            if (ehp > 0)
+            {
+                switch (random (1,2) + '0')
+                {
+                    case '1':
+                    {
+                        e_attack ("sword");
+                        break;
+                    }
+                    case '2':
+                    {
+                        e_attack ("magic");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                e2 += 15;
+                cout << "The enemy was annihilated. Experience with magic now is " << e2 << "." << endl;
+                win();
+                return 0;
+            }
+            if (hp <= 0)
+            {
+                lose();
+                return 0;
+            }
+                break;
+        }
+        else
+        {
+            nomana();
+            break;
+        }
+    }
+    default:
+    {
+        cout << "Ability not learned." << endl << endl;
         break;
-        }
-        case '2':
-        {
-        e_attack ("magic");
-        break;
-        }
-        }
-    }
-    else
-    {
-        win();
-        return 0;
-    }
-    if (hp <= 0)
-    {
-        lose();
-        return 0;
-    }
-}
-else
-    {
-        nomana();
-        goto label;
-    }
-break;
-}
-
-case '2':
-{
-if (mp >= _mcos2)
-{
-    p_attack ("magic");
-
-    if (ehp > 0)
-    {
-        switch (random (1,2) + '0')
-        {
-        case '1':
-        {
-        e_attack ("sword");
-        break;
-        }
-        case '2':
-        {
-        e_attack ("magic");
-        break;
-        }
-        }
-    }
-    else
-    {
-        win();
-        return 0;
     }
 
-    if (hp <= 0)
-    {
-        lose();
-        return 0;
-    }
-}
-else
-    {
-        nomana();
-        goto label;
-    }
-break;
-}
-
-case '3':
-{
-if (mp >= _mcos3)
-{
-    p_attack ("ult");
-
-    if (ehp > 0)
-    {
-        switch (random (1,2) + '0')
-        {
-        case '1':
-        {
-        e_attack ("sword");
-        break;
-        }
-        case '2':
-        {
-        e_attack ("magic");
-        break;
-        }
-        }
-    }
-    else
-    {
-        win();
-        return 0;
-    }
-
-    if (hp <= 0)
-    {
-        lose();
-        return 0;
-    }
-}
-else
-    {
-        nomana();
-        goto label;
-    }
-break;
-}
-
-default:
-{
-    cout << "Ability not learned." << endl << endl;
     system("pause");
-    goto label;
-    break;
-}
-}
-system("pause");
-goto label;
+} //switch
+
+} //while
 return 0;
-}
+} //main
+
